e802194fc6879f2fadda7b3b8a146eee468f9b52
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-25 13:35:44 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here:
+function driversWithRevenueOver(list, revenue) {
+  let a = list.filter(function (x) { return x["revenue"] > revenue });
+  return a
+}
+
+function driverNamesWithRevenueOver(list, revenue) {
+  let a = this.driversWithRevenueOver(list, revenue);
+  let b = a.map(function (x) {return x["name"]});
+  return b;
+}
+
+function exactMatch(list, obj) {
+  let objPreKey = Object.keys(obj);
+  let objKey = objPreKey[0];
+  let objVal = obj[objKey];
+  let a = list.filter(function (x) { return x[objKey] === objVal });
+  return a;
+}
+
+function exactMatchToList(list, obj) {
+  let a = this.exactMatch(list, obj);
+  let b = a.map(function (x) {return x["name"]});
+  return b;
+}
